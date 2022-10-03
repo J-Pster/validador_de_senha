8c5e37f7202f9cafb0d7fa36b9cab8c8da3113c0
J-Pster <J-Pster@users.noreply.github.com>
2022-10-03 02:46:53 +0000

    deploy: 74000277332c2db02dfbe3fbbdf9129732bd5269

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Lê Faça Login na tela!', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/faça login/i);
-  expect(linkElement).toBeInTheDocument();
-});
