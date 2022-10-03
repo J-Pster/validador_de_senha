e622ea29d747fee687539ee73c385297f794cc57
João Pster <joaopsterdev@gmail.com>
2022-10-02 23:28:30 -0300

    Adicionado o primeiro teste

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Lê Faça Login na tela!', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/faça login/i);
   expect(linkElement).toBeInTheDocument();
 });
